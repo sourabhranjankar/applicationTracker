3738e77964f93aa4e0e627abbb20947e51c54417
Sourabh <you@example.com>
2025-05-02 11:11:49 -0500

    Added Reset

--- script.js
+++ script.js
@@ -138,6 +138,12 @@ function exportToCSV() {
   link.click();
 }
 
+function reset_storage(){
+  localStorage.clear();
+  location.reload();
+}
+
+
 function setTodayAsDefaultDate() {
   const today = new Date().toISOString().split("T")[0];
   document.getElementById("date").value = today;
